af1ca753085fb5066a0a27a3c81165ab2df8fd00
trutnev.daniil <trutnev.daniil@khk.ee>
2021-11-11 17:15:08 +0200

    Commit and push1

---
+++ somefile.js
@@ -0,0 +1,9 @@
+document.getElementById("demo").addEventListener("click", myFunction);
+
+function myFunction() {
+    documentxc.getElementById("demo").innerHTML = "YOU CLICKED ME!";
+}document.getElementById("demo").addEventListener("click", myFunction);
+
+function myFunction() {
+    document.getElementById("demo").innerHTML = "YOU CLICKED ME!";
+}
